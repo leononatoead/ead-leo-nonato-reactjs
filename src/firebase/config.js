bf991aa68ca07400551a45791db2d06ba6227c00
yagoramires <yago.ramiresx@gmail.com>
2023-08-27 00:33:15 -0300

    fix: ajuste nas variaveis de ambiente

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -11,13 +11,13 @@ import { getAuth } from 'firebase/auth';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_VERCEL_API_KEY,
+  authDomain: import.meta.env.VITE_VERCEL_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_VERCEL_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_VERCEL_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_VERCEL_MESSAGE_SENDER,
+  appId: import.meta.env.VITE_VERCEL_APP_ID,
+  measurementId: import.meta.env.VITE_VERCEL_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
