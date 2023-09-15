a5959e9ee4d7017330dd7f5d80604893b4d66faf
yagoramires <yago.ramiresx@gmail.com>
2023-09-14 23:24:38 -0300

    feat: hook de posts

---
+++ src/hooks/usePosts.js
@@ -0,0 +1,116 @@
+import { useState } from 'react';
+import { database } from '../firebase/config';
+
+import { useDispatch } from 'react-redux';
+
+import {
+  Timestamp,
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from 'firebase/firestore';
+
+import { useToast } from '@chakra-ui/react';
+// import { delPost, editPost, newPost } from '../redux/modules/posts/actions';
+
+const usePosts = () => {
+  const [loading, setLoading] = useState(false);
+
+  const dispatch = useDispatch();
+  const toast = useToast();
+
+  const addPost = async (post) => {
+    setLoading(true);
+
+    try {
+      const postData = { ...post, createdAt: Timestamp.now() };
+
+      const res = await addDoc(collection(database, 'posts'), postData);
+
+      const data = {
+        id: res.id,
+        ...post,
+        createdAt: Timestamp.now().toMillis(),
+      };
+
+      // dispatch(newPost(data));
+
+      toast({
+        description: 'Postulário adicionado com sucesso!',
+        status: 'success',
+        duration: '3000',
+        isClosable: true,
+      });
+    } catch (error) {
+      toast({
+        description: error.message,
+        status: 'error',
+        duration: '3000',
+        isClosable: true,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updatePost = async (id, post) => {
+    setLoading(true);
+
+    try {
+      const postRef = doc(database, 'posts', id);
+      await updateDoc(postRef, post);
+
+      const data = { id, ...post };
+
+      // dispatch(editPost(data));
+
+      toast({
+        description: 'Postulário atualizado com sucesso!',
+        status: 'success',
+        duration: '3000',
+        isClosable: true,
+      });
+    } catch (error) {
+      console.log(error);
+      toast({
+        description: error.message,
+        status: 'error',
+        duration: '3000',
+        isClosable: true,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deletePost = async (id) => {
+    try {
+      const postRef = doc(database, `posts`, id);
+      await deleteDoc(postRef);
+
+      // dispatch(delPost(id));
+
+      toast({
+        description: 'Postulário removido com sucesso!',
+        status: 'success',
+        duration: '3000',
+        isClosable: true,
+      });
+    } catch (error) {
+      toast({
+        description: error.message,
+        status: 'error',
+        duration: '3000',
+        isClosable: true,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { addPost, updatePost, deletePost, loading };
+};
+
+export default usePosts;
