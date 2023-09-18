377cb3fe36fb02ecf75fb1c17c19670f1753013e
gabrielss97 <gabriel.ssalvador97@gmail.com>
2023-09-18 10:32:55 -0300

    tirei rotas

--- src/routes/UserUnAuthenticated.jsx
+++ src/routes/UserUnAuthenticated.jsx
@@ -18,12 +18,6 @@ export default function UserUnAuthenticatedRoutes() {
       <Route path="/register" element={<Register />} />
       <Route path="/faq" element={<FAQ />} />
 
-      {/* TIRAR DEPOIS */}
-      <Route path="/verify-phone" element={<VerifyPhone />} />
-      <Route path="/verify-email" element={<VerifyEmail />} />
-      <Route path="/verify-sucess" element={<VerifySucess />} />
-      {/* TIRAR DEPOIS */}
-
       <Route path="/course/:id" element={<Course />} />
       <Route path="/course/:id/:id" element={<CourseWatch />} />
       <Route path="/newsletter" element={<Newsletter />} />
