509ddc0ff8b8dcd75c0476e2bbc4b698593feff0
yagoramires <yago.ramiresx@gmail.com>
2023-10-16 20:20:52 -0300

    fix: corrigindo erro de verificacao de tamanho do array quando nao tem array

--- src/pages/Global/CourseWatch/VideoContent.jsx
+++ src/pages/Global/CourseWatch/VideoContent.jsx
@@ -33,10 +33,10 @@ export default function VideoContent({ videoData, setVideoData }) {
   return (
     <Box className='pt-2 '>
       <Box className='px-4 flex flex-col gap-[6px]'>
-        <Text className='text-small leading-4'>{videoData.section}</Text>
+        <Text className='text-small leading-4'>{videoData?.section}</Text>
         <Box className='flex items-center justify-between'>
           <Text className='text-base leading-5 font-bold'>
-            {videoData.title}
+            {videoData?.title}
           </Text>
           <button onClick={handleOpenDescription}>
             <RiArrowDownSLine
@@ -51,12 +51,12 @@ export default function VideoContent({ videoData, setVideoData }) {
             !showDescription && 'hidden'
           } my-2 text-small leading-4`}
         >
-          {videoData.description}
+          {videoData?.description}
         </Text>
       </Box>
 
       <Box className='flex items-center justify-center pt-[18px] pb-6 px-[22px] gap-[12px] border-b-[1px] border-b-gray-250'>
-        {videoData.assetsList.length > 0 && (
+        {videoData?.assetsList?.length > 0 && (
           <button
             onClick={handleSelectMaterials}
             className='rounded-md px-3 py-2 w-[97px] h-[60px] border-[1px] border-gray-250 bg-gray-250 flex flex-col items-center justify-center gap-1'
@@ -67,7 +67,7 @@ export default function VideoContent({ videoData, setVideoData }) {
             </Text>
           </button>
         )}
-        {videoData.questionsList.length > 0 && (
+        {videoData?.questionsList?.length > 0 && (
           <button
             onClick={handleSelectQuestions}
             className='rounded-md px-3 py-2 w-[97px] h-[60px] border-[1px] border-gray-250 bg-gray-250 flex flex-col items-center justify-center gap-1'
